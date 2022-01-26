259d402974f79ababe243caf42ae25800f4d2b20
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-26 11:52:39 +0200

    Initial Commit

---
+++ P11-01/P11_01App.swift
@@ -0,0 +1,17 @@
+//
+//  P11_01App.swift
+//  P11-01
+//
+//  Created by Vahtee Boo on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P11_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
